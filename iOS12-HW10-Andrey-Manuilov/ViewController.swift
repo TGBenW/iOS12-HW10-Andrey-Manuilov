f7dd4a8765efe4080047ef4e94630c001565e75d
Andrey Manuilov <dukariga@gmail.com>
2023-12-19 15:21:28 +0200

    Removed unnecessary comments

--- iOS12-HW10-Andrey-Manuilov/ViewController.swift
+++ iOS12-HW10-Andrey-Manuilov/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  iOS12-HW10-Andrey-Manuilov
-//
-//  Created by Andrey Manuilov on 19/12/2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
